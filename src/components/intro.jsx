29e5b84429df32a63f3228eaee2cee1e5af8d21f
Berkin Kınay <103454964+developedbyven@users.noreply.github.com>
2022-07-23 16:43:31 +0300

    Update intro.jsx

--- src/components/intro.jsx
+++ src/components/intro.jsx
@@ -8,22 +8,22 @@ import {SiLinkedin,
 
 function Intro() {
   return (
- <div class="flex xl:flex-col">
-  
-    <div className= 'w-1/2 h-screen text-white font-sans:ui-sans-serif p-16 xl:w-full' id="section1"> 
+<div class="flex xl:flex-col md:flex-col">
 
-        <h1 className='text-white-200 text-4xl px-32 mt-32 pt-36 pr-36 font-medium xl:flex items-center justify-center:left'>Hello There!</h1>  
-        <h2 className='text-4xl pl-32 pt-6 pb-4 font-normal xl:flex items-center justify-center:left'>I'm Berkin Kınay</h2>
+<div className= 'w-1/2 h-screen text-white p-16 md:h-auto md:items-center xl:w-full xl:h-auto xl:items-center xl:justify-center 2xl:h-auto' id="partone"> 
+      <div class="xl:p-96 xl:pl-48"></div>
+        <h1 className='text-white-200 text-4xl pt-72 pl-[130px] font-medium xl:flex items-center justify-center:left '>Hello There!</h1>    
+        <h2 className='text-4xl pl-32 pt-8 pb-6 font-normal items-center justify-center:left md:flex md:w-max md:h-full md:text-3xl'>I'm Berkin Kınay</h2>
   
-         <div className='w-full pl-48 text-white text-3xl xl:flex items-center justify-center:left'> 
+         <div className='w-full pl-48 text-white text-3xl items-center justify-center:left md:flex md:w-max md:h-max md:pl-[160px] max-w-max'> 
             <Typewriter  
               onInit={(typewriter) => {
                 typewriter
                   .pauseFor(1000)
                   .typeString ("Front-End Dev.")
-                  .pauseFor(3000)
+                  .pauseFor(4000)
                   .deleteAll()
-                  .typeString ("Web Designer")
+                  .typeString ("Web Developer")
                   .pauseFor(1000)
                   .deleteAll()
                   .typeString ("Videographer")
@@ -34,12 +34,12 @@ function Intro() {
               }}
             />
          </div>  
-        <div className='text-white-200 text-base-1rem  px-24 pt-8 pb-4 pr-16 font-medium object-cover xl:flex items-center justify-center:left'>I love creating and working on a new idea and that's why I'm here</div>
+        <div className='mb-12 text-white-200 text-base-1rem  px-24 pt-8 pb-4 pr-16 font-medium object-cover xl:flex items-center justify-center:left md:flex md:w-max md:text-xs md:h-max md:pl-[100px] 2xl:mb-24'>I love creating and working on a new idea and that's why I'm here</div>
 
-    <div className='mt-24 flex items-center justify-center pr-48 gap-16'>
-           <div className='text-black hover:dark:text-white pt-4 min-w-max min-h-max' onClick={() => {
+    <div className='flex 2xl:p-24 2xl:pt-12 items-center justify-center gap-16 md:w-full md:h-max md:items-center md:justify-center md:grid md:grid-col md:p-24 md:object-cover 2xl:object-cover '>
+           <div className='text-black hover:dark:text-white pt-4  pl-2min-w-max min-h-max' onClick={() => {
                      window.location.href= "https://www.linkedin.com/in/berkin-k%C4%B1nay-04a137228/"
-                   }}> <SiLinkedin size={56}/> </div>
+                   }}> <SiLinkedin size={62}/> </div>
 
            <div className='text-black hover:dark:text-white pt-4 pl-2 min-w-max min-h-max' onClick={() => {
                      window.location.href= "https://github.com/developedbyven"
@@ -48,18 +48,22 @@ function Intro() {
            <div className='text-black hover:dark:text-white pt-4 pr-2 min-w-max min-h-max' onClick={() => {
                       window.location.href= "https://www.instagram.com/berkinkinay/"
                    }}> <AiFillInstagram size={62}/></div>
-
       </div>
     </div>
 
-       <div className='w-1/2 flex h-screen items-center pr-[90px] justify-center xl:w-full xl:pt-[120px]'>  
+    <div className='md:h-auto md:w-auto md:object-cover md:pt-64 md:absolute top-0 3xl:hidden'> 
+              <img class="md:h-96 md:w-96 md:bg-black " src={Me} alt ="" className="lightside" />    
+    </div>
+
+    <div className='w-1/2 flex h-screen items-center pr-[90px] justify-center xl:w-full md:h-auto md:w-auto md:object-cover md:items-center md:justify-center md:absolute md:p-24 xl:absolute top-0 xl:p-10 md:hidden'> 
               <img class="h-96 w-96 bg-black " src={Me} alt ="" className="lightside" />    
-      
-             <div className='text-white absolute bottom-0 right-0 h-16 w-16' onClick={() => {
-                     window.location.href= "#section3"
+   </div>
+       
+       <div className='text-white absolute bottom-0 right-0 h-16 w-16 animate-bounce md:absolute-0 md:right-0 md:h-12' onClick={() => {
+                     window.location.href="#parthree"
                    }}>  <BsArrowDownCircleFill size={40} />{" "} </div>
-       </div>
    </div>
   );
 }
 export default Intro;
+      
