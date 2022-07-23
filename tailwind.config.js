b9824159c4bde04d1b05ffb5a20d6ad0356d1405
Berkin Kınay <103454964+developedbyven@users.noreply.github.com>
2022-07-23 16:47:00 +0300

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,19 +2,25 @@ module.exports = {
     content: ["./src/**/*.{html,js,jsx}"],
     theme: {
       screens: {
-        '2xl': {'max': '1540px'},
-      // => @media (max-width: 1535px) { ... }
+      '3xl': {'max': '21250px'},
+         // => @media (max-width: 1535px) { ... } 
+        
+      '2xl': {'max': '1540px'},
+        // => @media (max-width: 1535px) { ... } 
 
       'xl': {'max': '1279px'},
-      // => @media (max-width: 1279px) { ... }
+        // => @media (max-width: 1279px) { ... }
 
-      'lg': {'max': '1023px'},
+      'lg': {'max': '1000px'},
       // => @media (max-width: 1023px) { ... }
 
-      'md': {'max': '767px'},
+      'md': {'max': '768px'},
       // => @media (max-width: 767px) { ... }
 
-      'sm': {'max': '639px'},
+      'sm': {'max': '640px'},
+      // => @media (max-width: 639px) { ... }
+
+      'xs': {'max': '430px'},
       // => @media (max-width: 639px) { ... }
       },
     }
