786242a90b3bac5749df3ceac0e207de844974e3
Berkin Kınay <103454964+developedbyven@users.noreply.github.com>
2022-07-23 16:44:39 +0300

    Update about.jsx

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -17,30 +17,30 @@ import {
 } from "react-icons/si";
 import { FaNode } from "react-icons/fa";
 import Typewriter from "typewriter-effect";
-import {BsArrowDownCircleFill} from "react-icons/bs";
 
 function About () {
     return (
-        <div class='flex bg-zinc-600 h-screen xl:flex-col'>
+       <div class='flex bg-zinc-600 h-screen xl:flex-col flex-col md:flex-col md:h-auto xl:h-auto'>
 
  
-            <div className="h-1/2 w-full text-white  font-sans:ui-sans-serif " id="section2">
-                 <div className= 'h-1/4 w-full text-4xl pl-64 font-medium pt-16 p-16'>
+            <div className="h-1/2 w-full text-white">
+                 <div className= 'flex object-fill h-1 items-center justify-center:left text-4xl pl-56 font-medium pt-16 p-16 md:flex md:w-max md:h-max md:text-4xl'>
                           Briefly me,
                 </div>
 
-             <div className="h-1/2 pb-16 items-center justify-center font-normal text-white text-2xl pl-36 pr-36">
+             <div className="flex object-fill pb-1 items-center justify-center font-normal text-white text-2xl pl-36 pr-36">
 
-                     <h3 className=" text-white mt-12 text-1xl ml-12"> 
+                     <div className=" text-white text-1xl ml-12 md:text-base md:h-1 md:flex md:w-max"> 
                         I live in İzmir, Turkey and I'm 23. I am a 2nd year student of Computer Programming. I work on developing and editing new
                          ideas. I like to improve myself and learn new technologies. I believe
                          that teamwork is the key to do good all these things, we can do this
                          together. I love new creative things that might be in software and 
                          visual graphics. if you want to talk about anything
                          you can contact me...
-                     </h3> 
-                 
-                 <div className="flex justify-center:left text-white mt-16 pl-12 font-bold ml-12 text-2xl">
+                     </div> 
+            </div>
+
+            <div className="flex items-center justify-center text-white  text-2xl pt-16 font-bold md:flex md:items-center md:justify center md:w-max-screen md:text-sm md:max-w md:pt-80">
                     <Typewriter  
                         onInit={(typewriter) => {
                           typewriter
@@ -48,11 +48,10 @@ function About () {
                              .pauseFor(1000)
                              .start();
                          }}
-                       />
-                </div>
-            </div>
+                     />
+             </div>
 
-            <div className="flex items-center justify-center gap-20 pt-24">
+            <div className="flex items-center justify-center gap-20 pt-24 xl:grid xl:grid-col xl:h-screen xl:object-cover md:grid md:grid-col md:object-cover xl:mb-96">
                  <MdCode size="3em" color="purple" />{" "}
                     <SiJavascript size=" 3.5em" color="yellow" />{" "}
                     <FaNode size="4.2em" color="lime" />{" "}
@@ -68,7 +67,7 @@ function About () {
                 <BsCodeSlash size="3em" color="purple" />{" "}
             </div>
 
-                 <div className="flex justify-center text-white mt-24 text-2xl">
+                 <div className="flex items-center justify-center text-white mt-24 text-2xl sm:display-none md:text-sm xl:object-cover xl:pt-[600px]">
                     <Typewriter  
                         onInit={(typewriter) => {
                           typewriter
@@ -79,16 +78,13 @@ function About () {
                        />
                  </div>
 
-             <div className="mt-24 flex items-center justify-center gap-20 object-cover 2xl:object-cover xl:object-cover">
-                 <MdCode size="3em" color="purple" />{" "}
+             <div className="mt-12 flex items-center justify-center gap-20 object-cover 2xl:object-cover xl:object-cover xl:grid xl:grid-col xl:h-screen md:grid md:grid-col md:object-cover">
+                 <MdCode size="3.1em" color="purple" />{" "}
                     <SiVuedotjs size="4.6em" color="green" />{" "}
                      <SiFlutter size="4.6em" color="black" />{" "}
-                 <BsCodeSlash size="3em" color="purple" />{" "}
+                 <BsCodeSlash size="3.1em" color="purple" />{" "}
             </div>
-         <div className='text-white absolute bottom-0 right-0 h-16 w-16' onClick={() => {
-                     window.location.href= "#section3"
-                   }}>  <BsArrowDownCircleFill size={40} />{" "} </div>
-      
+    
      </div>
 </div>
 
