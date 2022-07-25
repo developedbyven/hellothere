7bf96c3bc5804e3edf00f3b0987cd00d1becd4e2
Berkin Kınay <103454964+developedbyven@users.noreply.github.com>
2022-07-25 16:49:10 +0300

    Update intro.jsx

--- src/components/intro.jsx
+++ src/components/intro.jsx
@@ -8,14 +8,15 @@ import {SiLinkedin,
 
 function Intro() {
   return (
-<div class="flex xl:flex-col md:flex-col">
+<div class="flex xl:flex-col md:flex-col md:h-auto xs:flex-col">
 
-<div className= 'w-1/2 h-screen text-white p-16 md:h-auto md:items-center xl:w-full xl:h-auto xl:items-center xl:justify-center 2xl:h-auto' id="partone"> 
-      <div class="xl:p-96 xl:pl-48"></div>
-        <h1 className='text-white-200 text-4xl pt-72 pl-[130px] font-medium xl:flex items-center justify-center:left '>Hello There!</h1>    
-        <h2 className='text-4xl pl-32 pt-8 pb-6 font-normal items-center justify-center:left md:flex md:w-max md:h-full md:text-3xl'>I'm Berkin Kınay</h2>
+   <div className= 'w-1/2 h-screen text-white p-16 xl:w-full xl:h-auto xl:items-center xl:justify-center 2xl:h-auto md:flex md:flex-col md:items-center md:justify-center md:w-full md:h-full' id="partone"> 
+
+    
+        <div className='xl:w-full flex text-white-200 text-4xl pt-72 pl-[130px] font-medium items-center justify-center:left xl:mt-96 xl:flex md:items-center justify-center:left md:flex md:w-max md:text-3xl md:h-max md:pl-[10px] xs:flex xs:items-center justify-center:left xs:w-max xs:text-3xl xs:h-max'>Hello There!</div>    
+        <div className='xl:w-full text-4xl pl-32 pt-8 pb-6 font-normal xl:flex items-center justify-center:left md:flex md:w-max md:text-2xl md:h-max md:pl-[10px] md:items-center justify-center:left xs:flex xs:w-max xs:text-2xl xs:h-max'>I'm Berkin Kınay</div>
   
-         <div className='w-full pl-48 text-white text-3xl items-center justify-center:left md:flex md:w-max md:h-max md:pl-[160px] max-w-max'> 
+         <div className='w-full pl-48 text-white text-3xl xl:flex items-center justify-center:left md:flex md:w-max md:text-xl md:h-max md:pl-[10px] xs:flex md:items-center justify-center:left xs:w-max xs:text-xl xs:h-max'> 
             <Typewriter  
               onInit={(typewriter) => {
                 typewriter
@@ -32,14 +33,15 @@ function Intro() {
                   .typeString ("Software Designer")
                   .start();
               }}
-            />
+            />  
          </div>  
-        <div className='mb-12 text-white-200 text-base-1rem  px-24 pt-8 pb-4 pr-16 font-medium object-cover xl:flex items-center justify-center:left md:flex md:w-max md:text-xs md:h-max md:pl-[100px] 2xl:mb-24'>I love creating and working on a new idea and that's why I'm here</div>
+        <div className='mb-12 text-white-200 text-base-1rem  px-24 pt-8 pb-4 pr-16 font-medium object-cover xl:flex items-center justify-center:left md:flex md:full md:text-xs md:h-max 2xl:mb-24 xs:flex xs:items-center justify-center xs:w-max-screen xs:text-xs xs:h-max'>
+                      I love creating and working on a new idea and that's why I'm here</div>
 
     <div className='flex 2xl:p-24 2xl:pt-12 items-center justify-center gap-16 md:w-full md:h-max md:items-center md:justify-center md:grid md:grid-col md:p-24 md:object-cover 2xl:object-cover '>
-           <div className='text-black hover:dark:text-white pt-4  pl-2min-w-max min-h-max' onClick={() => {
+           <div className='text-black hover:dark:text-white pt-4  pl-2 min-w-max min-h-max' onClick={() => {
                      window.location.href= "https://www.linkedin.com/in/berkin-k%C4%B1nay-04a137228/"
-                   }}> <SiLinkedin size={62}/> </div>
+                   }}> <SiLinkedin size={58}/> </div>
 
            <div className='text-black hover:dark:text-white pt-4 pl-2 min-w-max min-h-max' onClick={() => {
                      window.location.href= "https://github.com/developedbyven"
@@ -66,4 +68,3 @@ function Intro() {
   );
 }
 export default Intro;
-      
